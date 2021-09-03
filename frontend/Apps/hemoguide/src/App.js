0d7216ccdd82fd4ab9ed0543039972f199836383
Lili Tong <insa.lilitong@gmail.com>
2021-09-03 11:57:50 +0200

    update frontend

---
+++ frontend/Apps/hemoguide/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Watchlist from 'watchlist';
+
+function App() {
+  return (
+    <div className='App'>
+      <Watchlist />
+    </div>
+  );
+}
+
+export default App;
