eef6366042c53f23f9d7467f184ff751a4bb9020
Dennis Lomans <dennis.lomans@philips.com>
2021-09-06 13:57:02 +0200

    feat: Added project to test gradlew

---
+++ backend/demo/src/test/java/com/eye2web/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eye2web.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
