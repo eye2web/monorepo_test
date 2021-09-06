823ead823e564fe24d8144680812879bb265aefa
Remco van der Heijden <remco.van.der.heijden@philips.com>
2021-09-06 16:51:39 +0200

    ci: setup Dockerfile for demo backend

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -28,6 +28,9 @@ extract_packagejson_variables () {
     echo "Extracting component name from package.json"
     COMPONENT_NAME=$(node -pe 'JSON.parse(process.argv[1]).name' "$(cat package.json)")
 
+    echo "Extracting component group from package.json"
+    COMPONENT_GROUP=$(node -pe "(JSON.parse(process.argv[1]).group != undefined)? JSON.parse(process.argv[1]).group:'default'" "$(cat package.json)")
+
     if [ -z ${COMPONENT_VERSION+x} ]; then 
     echo "Extracting component version from package.json"
     COMPONENT_VERSION=$(node -pe 'JSON.parse(process.argv[1]).version' "$(cat package.json)")
@@ -35,19 +38,19 @@ extract_packagejson_variables () {
 };
 
 check_artifactory_image () {
-    echo "Checking if container with name ($COMPONENT_NAME) and version ($COMPONENT_VERSION) already exists"
+    echo "Checking if container ($COMPONENT_GROUP/$COMPONENT_NAME:$COMPONENT_VERSION) already exists"
     # Returns a json object that includes a list of versions (if exists)
     ARTIFACTORY_RESPONSE=$(curl -X GET $ARTIFACTORY_HOST/artifactory/api/docker/$ARTIFACTORY_REGISTRY/v2/$COMPONENT_NAME/tags/list -H X-JFrog-Art-Api:$ARTIFACTORY_API_KEY)
 
     # Returns true when version already exists in artifactory
     CONTAINS_VERSION=$(node -pe "let parsed = JSON.parse(process.argv[1]); (parsed.tags == undefined || !parsed.tags.includes(\"$COMPONENT_VERSION\"))? 'false':'true';" "$(echo $ARTIFACTORY_RESPONSE)")
-    echo "Container with name ($COMPONENT_NAME) version ($COMPONENT_VERSION) exists=($CONTAINS_VERSION)"
+    echo "Container ($COMPONENT_GROUP/$COMPONENT_NAME:$COMPONENT_VERSION) exists=($CONTAINS_VERSION)"
 };
 
 build_image () {
     # Build image
     echo "Building docker image with name: ($DOCKER_REPOSITORY$COMPONENT_NAME:$COMPONENT_VERSION)"
-    docker build --build-arg version=${COMPONENT_VERSION} --build-arg name=${COMPONENT_NAME} -t $DOCKER_REGISTRY$COMPONENT_NAME:$COMPONENT_VERSION $DOCKER_FILE
+    docker build --build-arg version=${COMPONENT_VERSION} --build-arg name=${COMPONENT_NAME} -t "$DOCKER_REGISTRY$COMPONENT_GROUP/$COMPONENT_NAME:$COMPONENT_VERSION" $DOCKER_FILE
 };
 
 push_image () {
