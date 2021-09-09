b288a751e782c8197a8b5800e5b12ded7f362876
Dennis Lomans <dennis.lomans@philips.com>
2021-09-09 13:00:25 +0200

    refactor: switch to docker version

---
+++ .github/actions/sonar-mono-repo/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z "${SONAR_TOKEN}" ]]; then
+  echo "This GitHub Action requires the SONAR_TOKEN env variable."
+  exit 1
+fi
+
+if [[ -z "${SONAR_HOST_URL}" ]]; then
+  echo "This GitHub Action requires the SONAR_HOST_URL env variable."
+  exit 1
+fi
+
+if [[ -f "${INPUT_PROJECTBASEDIR%/}package.json" ]]; then
+  echo "The given project root does not contain a package.json."
+  exit 1
+fi
+
+
+
+
+sonar-scanner -Dsonar.projectBaseDir=${} ${INPUT_ARGS} 
