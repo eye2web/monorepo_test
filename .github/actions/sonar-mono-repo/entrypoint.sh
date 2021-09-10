3c90d9d0cf1b20b30d821b6e57b632d82b1955de
Dennis Lomans <dennis.lomans@philips.com>
2021-09-10 15:28:12 +0200

    chore: test filepath extraction

--- .github/actions/sonar-mono-repo/entrypoint.sh
+++ .github/actions/sonar-mono-repo/entrypoint.sh
@@ -38,14 +38,14 @@ workspaces=( $(jq -r '.workspaces[]' "${SONAR_SOURCE_DIR}/package.json") )
 
 unset JAVA_HOME
 
+unset SONAR_JAVA_OPTS
+
 if test -n "$(find ${filepath} -maxdepth 15 -name '*.java' -print -quit)"
 then
     echo ${filepath}
     SONAR_JAVA_OPTS="-Dsonar.java.binaries=./build/classes/java/main \
                    -Dsonar.java.test.binaries=./build/classes/java/test \
                    -Dsonar.java.source=11"
-    else
-    SONAR_JAVA_OPTS=
 fi
 
 for filepath in "${workspaces[@]}" ; do
